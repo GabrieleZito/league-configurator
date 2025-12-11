ce44400fa38f5304009df13869044f80e8969fed
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2025-12-11 11:54:35 +0100

    feat: add more types, rename champion route

--- src/lib/api/datadragon.ts
+++ src/lib/api/datadragon.ts
@@ -1,4 +1,4 @@
-import { ChampionListResponse, ChampionSummary } from "@/types/datadragon";
+import { DDResponse, ChampionSummary, ChampionDetails } from "@/types/datadragon";
 import axios from "axios";
 
 const api = axios.create({
@@ -9,6 +9,13 @@ const version = "15.24.1";
 const language = "en_US";
 
 export async function getChampionList(): Promise<ChampionSummary[]> {
-    const res = await api.get<ChampionListResponse>(`https://ddragon.leagueoflegends.com/cdn/${version}/data/${language}/champion.json`);
+    const res = await api.get<DDResponse<ChampionSummary>>(`https://ddragon.leagueoflegends.com/cdn/${version}/data/${language}/champion.json`);
+    return Object.values(res.data.data);
+}
+
+export async function getChampionDetails(name: string): Promise<ChampionDetails[]> {
+    const res = await api.get<DDResponse<ChampionDetails>>(
+        `https://ddragon.leagueoflegends.com/cdn/${version}/data/${language}/champion/${name}.json`,
+    );
     return Object.values(res.data.data);
 }
--- src/types/datadragon.ts
+++ src/types/datadragon.ts
@@ -1,3 +1,10 @@
+export interface DDResponse<T> {
+    type: string;
+    format: string;
+    version: string;
+    data: Record<string, T>;
+}
+
 export interface ChampionSummary {
     version: string;
     id: string;
@@ -11,44 +18,101 @@ export interface ChampionSummary {
         magic: number;
         difficulty: number;
     };
-    image: {
-        full: string;
-        sprite: string;
-        group: string;
-        x: number;
-        y: number;
-        w: number;
-        h: number;
-    };
+    image: Image;
     tags: string[];
     partype: string;
-    stats: {
-        hp: number;
-        hpperlevel: number;
-        mp: number;
-        mpperlevel: number;
-        movespeed: number;
-        armor: number;
-        armorperlevel: number;
-        spellblock: number;
-        spellblockperlevel: number;
-        attackrange: number;
-        hpregen: number;
-        hpregenperlevel: number;
-        mpregen: number;
-        mpregenperlevel: number;
-        crit: number;
-        critperlevel: number;
-        attackdamage: number;
-        attackdamageperlevel: number;
-        attackspeedperlevel: number;
-        attackspeed: number;
-    };
+    stats: Stats;
 }
 
-export interface ChampionListResponse {
-    type: string;
-    format: string;
-    version: string;
-    data: Record<string, ChampionSummary>;
+export interface ChampionDetails {
+    id: string;
+    key: string;
+    name: string;
+    title: string;
+    image: Image;
+    skins: Skin[];
+    lore: string;
+    blurb: string;
+    allytips: string[];
+    enemytips: string[];
+    tags: string[];
+    partype: string;
+    stats: Stats;
+    spells: Spell[];
+    passive: Passive;
+    recommend: undefined[];
+}
+
+interface Image {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+}
+
+interface Skin {
+    id: string;
+    num: number;
+    name: string;
+    chromas: boolean;
+}
+
+interface Stats {
+    hp: number;
+    hpperlevel: number;
+    mp: number;
+    mpperlevel: number;
+    movespeed: number;
+    armor: number;
+    armorperlevel: number;
+    spellblock: number;
+    spellblockperlevel: number;
+    attackrange: number;
+    hpregen: number;
+    hpregenperlevel: number;
+    mpregen: number;
+    mpregenperlevel: number;
+    crit: number;
+    critperlevel: number;
+    attackdamage: number;
+    attackdamageperlevel: number;
+    attackspeedperlevel: number;
+    attackspeed: number;
+}
+
+interface Spell {
+    id: string;
+    name: string;
+    description: string;
+    tooltip: string;
+    leveltip: Leveltip;
+    maxrank: number;
+    cooldown: number[];
+    cooldownBurn: string;
+    cost: number[];
+    costBurn: string;
+    datavalues: Datavalues;
+    effect: number[] | undefined[];
+    effectBurn: string | undefined[];
+    vars: any[];
+    costType: string;
+    maxammo: string;
+    range: number[];
+    rangeBurn: string;
+    image: Image;
+    resource: string;
+}
+interface Leveltip {
+    label: string[];
+    effect: string[];
+}
+interface Datavalues {}
+
+interface Passive {
+    name: string;
+    description: string;
+    image: Image;
 }
