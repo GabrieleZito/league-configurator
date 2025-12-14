680adc8dd4abd67c2cfd7b3b3085936669a8ac75
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2025-12-14 10:25:51 +0100

    feat: add img domain to config, show image per champ

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,13 +1,21 @@
 import { getChampionList } from "@/lib/api/datadragon";
+import Image from "next/image";
 
 async function ChampionsPage() {
     const championList = await getChampionList();
 
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
+            <main className="flex min-h-screen w-full max-w-6xl flex-row flex-wrap items-center justify-between bg-zinc-50 px-16 py-32 sm:items-start dark:bg-black">
                 {championList.map((c) => (
-                    <div>{c.id}</div>
+                    <div key={c.id}>
+                        <Image
+                            src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${c.id}_0.jpg`}
+                            width={150}
+                            height={50}
+                            alt="champion image"
+                        />
+                    </div>
                 ))}
             </main>
         </div>
